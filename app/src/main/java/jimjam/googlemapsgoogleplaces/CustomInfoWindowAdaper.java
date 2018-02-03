08d05918ca1c198bc17a2b05e2da0498cebeacb1
odjuret <sundberg.jimmie@gmail.com>
2018-02-03 18:25:58 +0000

    All functional requirements except 1.6 (according to p15241925 First Deliverable.doc created and working.

---
+++ app/src/main/java/jimjam/googlemapsgoogleplaces/CustomInfoWindowAdaper.java
@@ -0,0 +1,53 @@
+package jimjam.googlemapsgoogleplaces;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+/**
+ * Created by Jimmie on 02/02/2018.
+ */
+
+public class CustomInfoWindowAdaper implements GoogleMap.InfoWindowAdapter{
+
+    private final View mWindow;
+    private Context mContext;
+
+    public CustomInfoWindowAdaper(Context context) {
+        mContext = context;
+        mWindow = LayoutInflater.from(context).inflate(R.layout.custom_info_window, null);
+    }
+
+    private void renderWindowText(Marker marker, View view){
+
+        String title = marker.getTitle();
+        TextView tvTitle = (TextView) view.findViewById(R.id.title);
+
+        if (!title.equals("")) {
+            tvTitle.setText(title);
+        }
+
+        String snippet = marker.getSnippet();
+        TextView tvSnippet = (TextView) view.findViewById(R.id.snippet);
+
+        if (!snippet.equals("")) {
+            tvSnippet.setText(snippet);
+        }
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        renderWindowText(marker, mWindow);
+        return mWindow;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        renderWindowText(marker, mWindow);
+        return mWindow;
+    }
+}
--- app/src/main/java/jimjam/googlemapsgoogleplaces/MapActivity.java
+++ app/src/main/java/jimjam/googlemapsgoogleplaces/MapActivity.java
@@ -3,7 +3,12 @@ package jimjam.googlemapsgoogleplaces;
 import android.Manifest;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,6 +16,15 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.ResolvableApiException;
@@ -28,10 +42,29 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Jimmie on 26/01/2018.
@@ -58,12 +91,32 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private static final String REQUESTING_LOCATION_UPDATES_KEY = "REQUESTING_LOCATION_UPDATES_KEY_SAVED";
     private Boolean mRequestingLocationUpdates = false;
 
+    //widgets
+    private EditText mSearchText;
+    private ImageView mGps;
+    private Spinner mSpinner;
+    private Polyline polyline = null;
+
     //methods
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onMapReady: map is ready");
         mMap = googleMap;
+
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
 
         if (mLocationPermissionGranted) {
             createLocationRequest();
@@ -80,20 +133,56 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             UiSettings uiSettings = mMap.getUiSettings();
             uiSettings.setAllGesturesEnabled(true);
             uiSettings.setCompassEnabled(true);
-            uiSettings.setMyLocationButtonEnabled(true);
+            uiSettings.setMyLocationButtonEnabled(false);
             uiSettings.setMapToolbarEnabled(true);
             uiSettings.setZoomControlsEnabled(true);
             mMap.setMyLocationEnabled(true);
+            mMap.setInfoWindowAdapter(new CustomInfoWindowAdaper(MapActivity.this));
+            init();
 
+            //populating with map markers
+            populateMap();
         }
+    }
+
+    private void populateMap(){
+        mMap.clear();
+        MarkerOptions options = new MarkerOptions()
+                .position(new LatLng(52.629381,-1.138030))
+                .title("GH - Gateway House")
+                .snippet("ablbalabllbalablba" + "\n" +
+                        "ablbalabllbalablba" + "\n" +
+                        "ablbalabllbalablba" + "\n");
+
+        mMap.addMarker(options);
     }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        mSearchText = (EditText) findViewById(R.id.input_search);
+        mGps = (ImageView) findViewById(R.id.ic_gps);
+        mSpinner = (Spinner) findViewById(R.id.spinner1);
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemSelected: selected item: " +parent.getItemAtPosition(position).toString());
+                if (parent.getItemAtPosition(position).toString().contains("GH")) {
+                    moveCamera(new LatLng(52.629381,-1.138030), DEFAULT_ZOOM);
+
+                    showDirections(new LatLng(52.629381,-1.138030));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         getLocationPermission();
+
         if (savedInstanceState != null){
             updateValuesFromBundle(savedInstanceState);
         }
@@ -128,6 +217,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         if (mRequestingLocationUpdates) {
             startLocationUpdates();
         }
+        clearPolylines();
     }
     private void updateValuesFromBundle(Bundle savedInstanceState) {
         // Update the value of mRequestingLocationUpdates from the Bundle.
@@ -140,6 +230,156 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         // Update UI to match restored state
         // updateUI();
+    }
+    private void init(){
+        Log.d(TAG, "init: initializing");
+
+        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || event.getAction() == KeyEvent.ACTION_DOWN
+                        || event.getAction() == KeyEvent.KEYCODE_ENTER){
+
+                    //execute our method for searching
+                    geoLocate();
+                }
+
+                return false;
+            }
+        });
+
+        mGps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked gps icon");
+                getDeviceLocation();
+            }
+        });
+
+        hideSoftKeyboard();
+    }
+
+    private void geoLocate(){
+        Log.d(TAG, "geoLocate: geo locating");
+
+        String searchString = mSearchText.getText().toString();
+
+        Geocoder geocoder = new Geocoder(MapActivity.this);
+        List<Address> list = new ArrayList<>();
+        try {
+            list = geocoder.getFromLocationName(searchString, 1);
+        }catch (IOException e){
+            Log.e(TAG, "geoLocate: IOException: " + e.getMessage() );
+        }
+
+        if (list.size() > 0) {
+            Address address = list.get(0);
+
+            Log.d(TAG, "geoLocate: found a location: " + address.toString());
+
+            moveCamera(new LatLng(address.getLatitude(), address.getLongitude()), DEFAULT_ZOOM);
+        }
+    }
+
+    /**
+     * Call this method to draw a polyline between current location and @param latLng.
+     * A polyline being the calculated best travel route by google places API.
+     * @param latLng        Destinations Lat Long object
+     */
+    private void showDirections(final LatLng latLng){
+        Log.d(TAG, "showDirections: method called");
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try {
+            if (mLocationPermissionGranted) {
+
+                Task location = mFusedLocationProviderClient.getLastLocation();
+
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful() && task.getResult() != null) {
+                            Log.d(TAG, "showDirections: onComplete: found location!");
+
+                            Location currentLocation = (Location) task.getResult();
+
+                            String url = getRequestUrl(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), latLng);
+                            TaskRequestDirections taskRequestDirections = new TaskRequestDirections();
+                            taskRequestDirections.execute(url);
+                        } else {
+                            Log.d(TAG, "showDirections: onComplete: last known location is null");
+                            Toast.makeText(MapActivity.this, "unable to get last know current location", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage());
+        }
+
+//        String url = getRequestUrl(new LatLng(location.getLatitude(), location.getLongitude()), latLng);
+//        TaskRequestDirections taskRequestDirections = new TaskRequestDirections();
+//        taskRequestDirections.execute(url);
+
+
+    }
+    
+    private String getRequestUrl(LatLng origin,LatLng dest){
+        //value of origin
+        String str_org = "origin=" +origin.latitude +"," +origin.longitude;
+        //value of destination
+        String str_dest = "destination=" + dest.latitude +"," +dest.longitude;
+        //set value enable the sensor
+        String sensor = "sensor=false";
+        //mode for find direction
+        String mode = "mode=driving";
+        //build the full param
+        String param = str_org + "&" + str_dest + "&" + sensor + "&" +mode;
+        //Output format
+        String output = "json";
+        //create url to request
+        String url = "https://maps.googleapis.com/maps/api/directions/" +output +"?" + param;
+        return url;
+    }
+
+    private String requestDirection(String reqUrl) throws IOException {
+        String responseString = "";
+        InputStream inputStream = null;
+        HttpURLConnection httpURLConnection = null;
+        try {
+            URL url = new URL(reqUrl);
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.connect();
+
+            //get the response result below
+            inputStream = httpURLConnection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader((inputStream));
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            StringBuffer stringBuffer = new StringBuffer();
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuffer.append(line);
+            }
+
+            responseString = stringBuffer.toString();
+            bufferedReader.close();
+            inputStreamReader.close();
+
+        } catch (Exception e) {
+            Log.e(TAG, "requestDirection: ", e);
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            httpURLConnection.disconnect();
+        }
+        return responseString;
     }
 
     private void getDeviceLocation() {
@@ -245,6 +485,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private void moveCamera(LatLng latLng, float zoom){
         Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + "   lng: " + latLng.longitude );
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+        hideSoftKeyboard();
     }
 
     //////////////////////////////////////
@@ -254,19 +495,22 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
                                 Manifest.permission.ACCESS_COARSE_LOCATION};
 
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-                mLocationPermissionGranted = true;
-                initMap();
+        //loop below to ensure not crashing on inital permission fail
+        while (!mLocationPermissionGranted){
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                if (ContextCompat.checkSelfPermission(this.getApplicationContext(), COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    mLocationPermissionGranted = true;
+                    initMap();
+                }else {
+                    ActivityCompat.requestPermissions(this,
+                            permissions,
+                            LOCATION_PERMISSION_REQUEST_CODE);
+                }
             }else {
                 ActivityCompat.requestPermissions(this,
                         permissions,
                         LOCATION_PERMISSION_REQUEST_CODE);
             }
-        }else {
-            ActivityCompat.requestPermissions(this,
-                    permissions,
-                    LOCATION_PERMISSION_REQUEST_CODE);
         }
     }
 
@@ -292,8 +536,92 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 }
             }
         }
+    }
+
+    private void hideSoftKeyboard(){
+        //this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+    private void clearPolylines(){
+        if (polyline != null) {
+            polyline.remove();
+        }
 
     }
 
+    /* ----------------------------- inner classes below -----------------------------------------  */
 
+    public class TaskRequestDirections extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.d(TAG, "TaskRequestDirections: doInBackground: called");
+            String responseString = "";
+
+            try {
+                responseString = requestDirection(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            //Parse json here
+            TaskParser taskParser = new TaskParser();
+            taskParser.execute(s);
+        }
+    }
+
+    public class TaskParser extends AsyncTask<String, Void, List<List<HashMap<String, String>>> > {
+
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... strings) {
+            JSONObject jsonObject = null;
+            List<List<HashMap<String, String>>> routes = null;
+            try {
+                jsonObject = new JSONObject(strings[0]);
+                DirectionsParser directionsParser = new DirectionsParser();
+                routes = directionsParser.parse(jsonObject);
+            } catch (JSONException e) {
+                Log.d(TAG, "TaskParser: doInBackground: " + e.getMessage());
+
+            }
+            return routes;
+        }
+
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> lists) {
+            //get list route and display it into the map
+
+            ArrayList points = null;
+
+            PolylineOptions polylineOptions = null;
+
+            for (List<HashMap<String, String>> path : lists) {
+                points = new ArrayList();
+                polylineOptions = new PolylineOptions();
+
+                for (HashMap<String, String> point : path) {
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lon = Double.parseDouble(point.get("lon"));
+
+                    points.add(new LatLng(lat, lon));
+                }
+
+                polylineOptions.addAll(points);
+                polylineOptions.width(15);
+                polylineOptions.color(Color.BLUE);
+                polylineOptions.geodesic(true);
+            }
+
+            if (polylineOptions!=null) {
+                polyline = mMap.addPolyline(polylineOptions);
+
+            } else {
+                Log.d(TAG, "TaskParser: onPostExecute: directions not found");
+            }
+        }
+    }
 }
